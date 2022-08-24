811d9436efc8921ffd8c15f0b34ee5d86b818586
sanaderer <sabrinanaderer@hotmail.com>
2022-08-24 11:49:01 -0300

    table products

--- src/pages/components/produtos/listagem/index.tsx
+++ src/pages/components/produtos/listagem/index.tsx
@@ -1,13 +1,21 @@
 import { Layout } from '../../../components'
 import Link from 'next/link'
+import { ProductTable } from './tabela'
+import { Product} from 'app/models/products'
 
 export const ProductsListing: React.FC = () => {
+
+    const products: Product[] = [{
+        id: "1", sku: "HGUTW67", name: "Impressora", price: 5000
+    }]
+
     return (
         <Layout title="Produtos" >
             <Link href= "/cadastros/produtos">
             <button className="button is-black">Novo</button>
             </Link>
             <br />
+            <ProductTable products={products} />
             
         </Layout>
 
---
+++ src/pages/components/produtos/listagem/tabela/index.tsx
@@ -0,0 +1,47 @@
+import { Product } from 'app/models/products'
+
+interface ProductsTableProps {
+    products: Array<Product>;
+}
+
+export const ProductTable: React.FC<ProductsTableProps> = ({
+    products
+}) => {
+    return (
+        <table className="table is-hoverable is-fullwidth is-striped">
+            <thead>
+                <th>Código</th>
+                <th>SKU</th>
+                <th>Nome</th>
+                <th>Preço</th>
+                <th></th>
+            </thead>
+            <tbody>
+                {
+                    products?.map( product => <ProductRow key={product.id} product={product} />)
+                }
+            </tbody>
+        </table>
+    )
+}
+
+interface ProductRowProps {
+    product: Product;
+}
+
+const ProductRow: React.FC<ProductRowProps> = ({
+    product
+}) => {
+    return(
+        <tr>
+            <td>{ product.id }</td>
+            <td>{ product.sku }</td>
+            <td>{ product.name }</td>
+            <td>{ product.price }</td>
+            <td>
+                <button className="button is-black"> Editar </button>
+                <button className="button is-light"> Deletar </button>
+            </td>
+        </tr>
+    )
+}
